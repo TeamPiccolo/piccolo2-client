a99c9b4d576323a23d263cf0c10ca2d2c29e8b70
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-02-26 16:38:39 +0000

    hook up scheduler

--- piccolo-client.py
+++ piccolo-client.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 from piccolo_client import PiccoloJSONRPCClient
+import datetime
 
 if __name__ == '__main__':
     
@@ -8,4 +9,8 @@ if __name__ == '__main__':
     
     print piccolo.components
     print piccolo.piccolo.info()
-    
+    print piccolo.piccolo.ping()
+
+    at = datetime.datetime.now()+datetime.timedelta(seconds=5)
+    end = at+datetime.timedelta(seconds=30)
+    print piccolo.piccolo.ping(at_time=at.isoformat(),interval=5.,end_time=end.isoformat())
