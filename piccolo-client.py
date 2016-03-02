4bd1c14ca55a85076fca007459fc15b947d24d99
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-03-02 10:57:09 +0000

    allow server URL to be specified

--- piccolo-client.py
+++ piccolo-client.py
@@ -2,10 +2,15 @@
 
 from piccolo_client import PiccoloJSONRPCClient
 import datetime
+import argparse
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u','--piccolo-url',metavar='URL',default='http://localhost:8080',help='set the URL of the piccolo server, default http://localhost:8080')
+    args = parser.parse_args()
     
-    piccolo = PiccoloJSONRPCClient("http://localhost:8080")
+    piccolo = PiccoloJSONRPCClient(args.piccolo_url)
     
     print piccolo.components
     print piccolo.piccolo.info()
