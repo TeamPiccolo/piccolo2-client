fc4ca3384ff42ccb59e2b23572a742200975162c
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-03-03 12:27:12 +0000

    use data directory

--- piccolo-client.py
+++ piccolo-client.py
@@ -5,8 +5,12 @@ import datetime
 
 if __name__ == '__main__':
     
-    piccolo = PiccoloJSONRPCClient("http://localhost:8080")
+    #piccolo = PiccoloJSONRPCClient("http://localhost:8080")
+    piccolo = PiccoloJSONRPCClient("http://129.215.6.10:8080")
     
+    #if not piccolo.piccolo.isMountedDataDir():
+    #    piccolo.piccolo.mountDatadir()
+
     print piccolo.components
     print piccolo.piccolo.info()
     print piccolo.piccolo.ping()
@@ -14,3 +18,8 @@ if __name__ == '__main__':
     at = datetime.datetime.now()+datetime.timedelta(seconds=5)
     end = at+datetime.timedelta(seconds=30)
     print piccolo.piccolo.ping(at_time=at.isoformat(),interval=5.,end_time=end.isoformat())
+
+    print piccolo.piccolo.isMountedDataDir()
+
+    if piccolo.piccolo.isMountedDataDir():
+        piccolo.piccolo.umountDatadir()
