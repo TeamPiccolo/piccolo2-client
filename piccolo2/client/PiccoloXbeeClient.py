65b8c1733dc7caa21f8613dcf82f0aa1df865df6
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-04-28 11:26:24 +0100

    move xbee communication into separate thread

--- piccolo2/client/PiccoloXbeeClient.py
+++ piccolo2/client/PiccoloXbeeClient.py
@@ -3,24 +3,67 @@ __all__ = ['PiccoloXbeeClient']
 from PiccoloBaseClient import PiccoloBaseClient
 
 from piccolo2.hardware import radio
+from piccolo2.PiccoloWorkerThread import PiccoloWorkerThread
+import threading
+from Queue import Queue
+import logging
+
 import json
 import time
 import zlib
 
+class XbeeClientThread(PiccoloWorkerThread):
+    """xbee client thread"""
+
+    LOGNAME = 'xbeeClient'
+
+    def __init__(self,address,panid,busy,tasks,results):
+        PiccoloWorkerThread.__init__(self,'xbee',busy,tasks,results)
+
+        self._rd = radio.APIModeRadio(panId=panid)
+        self._snr = self._rd.getSerialNumber()
+        self._address = address
+        
+    def run(self):
+        while True:
+            # wait for a new task from the task queue
+            task = self.tasks.get()
+            if task == None:
+                self.log.info('shutting down')
+                return
+            else:
+                command,component,keywords = task
+                cmd = json.dumps((self._snr,command,component,keywords))
+
+                self.busy.acquire()
+                self._rd.writeBlock(cmd,self._address)
+
+                result = self._rd.readBlock()
+                self.results.put(json.loads(result))
+                self.busy.release()
+                
 class PiccoloXbeeClient(PiccoloBaseClient):
     """communication via radio link"""
 
-    CHUNK = 100
-    
     def __init__(self,address,panid='2525'):
         """
         :param address: the address of the remote server
         :param panid: the panid"""
 
-        self._rd = radio.APIModeRadio(panId=panid)
-        self._snr = self._rd.getSerialNumber()
-        self._address = address
+        
+        self._busy = threading.Lock()
+        self._tQ = Queue()
+        self._rQ = Queue()
+        self._xbeeWorker = XbeeClientThread(address,panid,self._busy,self._tQ,self._rQ)
+
+        self._xbeeWorker.start()
         PiccoloBaseClient.__init__(self)
+        
+
+    def __del__(self):
+        # send poison pill to worker
+        self._tQ.put(None)
+        #PiccoloBaseClient.__del__()
 
     def invoke(self,command,component=None,keywords={}):
         """method used to call remote procedures
@@ -36,10 +79,12 @@ class PiccoloXbeeClient(PiccoloBaseClient):
         :rtype: (str,obj)
         """
 
-        cmd = json.dumps((self._snr,command,component,keywords))
+        if command == 'status' and component == 'piccolo':
+            # catch the status command and divert if xbee is busy
+            if self._busy.locked():
+                return 'communicating'
 
-        self._rd.writeBlock(cmd,self._address)
+        self._tQ.put((command,component,keywords))
 
-        result = self._rd.readBlock()
+        return self._rQ.get()
                 
-        return json.loads(result)
